b1778dc85816cd4a387c55d88905b3c491022422
luqs1 <luqmaanahmed@hotmail.com>
2019-06-19 11:24:09 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,15 @@
-import unittest
+import logtest
+
+invigilator = logtest.Test()
 
 testExpected = {"athena.log.1": 10, "athena.log.2": 10, "athena.log": 0}
 testOutcome = testExpected.copy()
 
 
 def testlog(logName, expectedValue):
-	return(assertEqual(RESULTOFTEST, expectedValue)) # fix this
+    realValue = invigilator.testcase(logName,config='t.conf')
+    return (realValue == expectedValue) # fix this
 
-#test = Test() 
-#test.testlog()
 
-for k, v in testLogs.items():
+for k, v in testExpected.items():
 	testOutcome[k] = testlog(k, v)
